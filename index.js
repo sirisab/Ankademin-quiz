df8afb17f7e046cd4481660a907e766608c25bc5
Siri Ahmed Backström <siri.ahmed.backstrom@gmail.com>
2023-12-05 16:13:22 +0100

    fråga ett text och knappar

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-//Deklarera knappar
+//Array med frågor
+myQuestions = [
+  {
+    id: 1,
+    question: "Vad heter Chiles största ö?",
+    answers: {
+      a: "Chileo",
+      b: "Chiloé",
+    },
+    correctAnswer: "b",
+  },
+  {
+    id: 2,
+    question: "Vad heter ön utanför Sveriges största ö?",
+    answers: {
+      a: "Fårö",
+      b: "Färö",
+    },
+    correctAnswer: "a",
+  },
+  {
+    id: 3,
+    question: "Vad heter ön i Siljan?",
+    answers: {
+      a: "Sållerö",
+      b: "Selleri-ön",
+    },
+    correctAnswer: "a",
+  },
+];
 
-question1BtnA = document.querySelector("#question1BtnA");
-question1BtnB = document.querySelector("#question1BtnB");
+//Initialisera index, en slags räknare
+let currentQuestionIndex = 0;
 
-//Clickevent knappar
+//Visa frågan
+let showQuestion = (array) => {
+  let quizContainer = document.querySelector("#quizContainer");
+  let text = document.createElement("p");
+  text.innerText = `${array[currentQuestionIndex].id} ${array[currentQuestionIndex].question}`;
+  quizContainer.append(text);
+};
 
-question1BtnA.addEventListener("click", () => {
-  alert("Du har fel!");
-});
+let showAnswers= (array) => {
+  let answerContainer = document.querySelector("#answerContainer");
+  let buttonA = document.createElement("button");
+  let buttonB = document.createElement("button");
+  buttonA.innerText = `${array[currentQuestionIndex].answers.a}`;
+  buttonB.innerText = `${array[currentQuestionIndex].answers.b}`;
+  answerContainer.append(buttonA, buttonB);
+};
 
-question1BtnB.addEventListener("click", () => {
-  alert("Du har rätt!");
-});
-//
+showQuestion(myQuestions);
+showAnswers(myQuestions);
