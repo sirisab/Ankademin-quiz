7e9b013486d6c9824c65c9cdac7f2850537c8280
sirisab <143996815+sirisab@users.noreply.github.com>
2023-12-12 23:30:00 +0100

    Lagt till knappar med mera

--- index.js
+++ index.js
@@ -2,23 +2,78 @@ const questions = [
   {
     id: 1,
     question: "Banan är ett frö",
-    a: "Sant",
-    b: "Falskt",
+    answers: {
+      a: "Sant",
+      b: "Falskt",
+    },
     correctAnswer: ["a"],
-    type: "radio,"
+    type: "radio",
   }, 
   {
     id: 2,
     question: "Trollpipistrell är en slags fladdermus",
-    a: "Sant",
-    b: "Falskt",
+    answers: {
+      a: "Sant",
+      b: "Falskt",
+    },
     correctAnswer: ["a"],
+    type: "radio",
   },
   {
     id: 3,
     question: "Liljekonvaljen är ätbar",
-    a: "Sant",
-    b: "Falskt",
+    answers: {
+      a: "Sant",
+      b: "Falskt",
+    },
     correctAnswer: ["b"],
+    type: "radio",
 }
 ]
+
+//Deklarerar och definierar variabler
+const startBtn = document.querySelector("#startBtn");
+const questionCard = document.querySelector("#questionCard");
+const nextBtn = document.querySelector("#nextBtn");
+let currentQuestionIndex;
+
+//Vad som händer när man klickar på start-knappen
+startBtn.addEventListener("click", () => {
+  
+  //Gömmer/visar olika element
+  startBtn.classList.add("hide");
+  questionCard.classList.remove("hide");
+  nextBtn.classList.remove("hide");
+  
+  currentQuestionIndex = 0;
+
+  //För varje fråga
+  questions.forEach ((question) => {
+  //Här sparar jag svaren:
+  let answers =[];
+  
+  //Och ifall frågan är av typen radio:
+  if (question.type === "radio") {
+
+    //Lägg till själva frågan
+    document.getElementById("questionCard").innerHTML = 
+      `Fråga ${question.id}: ${question.question}` 
+    }
+  });
+    // ... eller en checkbox.
+  });
+
+  //När man klickar på Next Button
+  nextBtn.addEventListener("click", (question) => {
+    currentQuestionIndex++;
+    document.getElementById("questionCard").innerHTML = 
+      `Fråga ${question.id}: ${question.question}`;
+      console.log(question.id);
+  })
+
+
+
+
+
+  
+  
